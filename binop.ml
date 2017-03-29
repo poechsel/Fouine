9bc3ea4fba2192b6b6488c6fa6b7202561eae8f6
glib <guillaume.duboc@ens-lyon.fr>
2017-03-29 12:02:04 +0200

    compil

--- binop.ml
+++ binop.ml
@@ -5,6 +5,8 @@ class ['e] binOp (print_symbol : string) (action : 'e -> 'e -> 'e)  = object
 
   method print a b = Printf.sprintf "%s %s %s" a print_symbol b
 
+  method symbol = print_symbol
+
   method interpret a b  = (action a b ) 
 
 end ;;
--- main.ml
+++ main.ml
@@ -2,6 +2,8 @@ open Expr
 open Env
 open Parser
 open Interpret
+open Compil
+open Binop
 
 let _ = print_endline "fouine interpreter"
 let _ = print_endline (if (let x = true in x && x) then "test" else "fail")
@@ -13,7 +15,7 @@ let _ = print_endline ""
 let _ = print_int (g' 4 2)
 let h a b () = a + b
                *)
-
+(*
 let lexbuf = Lexing.from_channel stdin
 
 (* on enchaîne les tuyaux: lexbuf est passé à Lexer.token,
@@ -24,4 +26,13 @@ let r = parse ()
 let _ = print_endline @@ beautyfullprint r
 let res, _ = interpret r (fun x y -> x, y) (fun x y -> x, y)
 let _ = print_endline @@ beautyfullprint res
+*)
+
+
+let e2 = In(Const 1, Const 2)
+let e3 = BinOp(addOp,Const 1, Const 2)
+let l2 = compile e3 
+let _ = print_endline @@ print_code l2
+
+
 
