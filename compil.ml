7d7e8dc6e098d834e5673f4408259ac33b8be900
glib <guillaume.duboc@ens-lyon.fr>
2017-04-03 02:16:45 +0200

    git fail, this is exec

--- compil.ml
+++ compil.ml
@@ -1,14 +1,14 @@
 open Expr
 open Binop
 open Env
-
-(* possible : compter les réfs vers les éléments de stack et dump et faire du garbage collecting *)
+open Stack
+(* possible : compter les réfs vers les éléments de s et  et faire du garbage collecting *)
 
 type instr = 
     C of int 
     | BOP of expr binOp 
     | ACCESS of string 
-    | CLOSURE of code
+    | CLOS of string*code
     | LET
     | ENDLET
     | APPLY
@@ -24,51 +24,86 @@ let rec print_code code =
 and print_instr i =
     match i with
         | C k -> Printf.sprintf "CONST(%s); " @@ string_of_int k
-        | BOP bop -> bop # symbol
+        | BOP bop -> bop # symbol ^ "; "
         | ACCESS s -> Printf.sprintf "ACCESS(%s); " s
-        | CLOSURE c -> Printf.sprintf "CLOSURE(%s); APPLY" @@ print_code c
+        | CLOS (s, c) -> Printf.sprintf "CLOSURE(%s, %s); " s (print_code c)
         | LET -> Printf.sprintf "LET; "
         | ENDLET -> Printf.sprintf "ENDLET; "
         | RETURN -> Printf.sprintf "RETURN; "
         | APPLY -> Printf.sprintf "APPLY; "
+        | PRINTIN -> Printf.sprintf "PRINTIN; "
 
 let rec compile = function
     | Const k -> [C k]
     | Unit -> []
     | Ident id -> [ACCESS id]
     | BinOp (op, e1, e2, _) -> (compile e2) @ (compile e1) @ [BOP op] 
+    | Fun (id, e, _) -> begin
+                          match id with
+                          | Ident(x) -> [CLOS (x, (compile e) @ [RETURN] )]
+                          | _ -> failwith "wrong identifier"
+                        end
     | Let (a, b, _) -> (compile a) @ [LET] @ (compile b) @ [ENDLET]
     | In(a, b, _) -> begin
                     match a with
-                        | Let (_, _, _) -> (compile a) @ [LET] @ (compile b) @ [ENDLET] 
+                        | Let _ -> (compile a) @ [LET] @ (compile b) @ [ENDLET] 
                         | _ -> (compile a) @ (compile b) @ [APPLY]
                   end 
-    | Fun (id, e, _) -> [CLOSURE ( (compile e) @ [RETURN] )]
-    | IfThenElse (cond, a, b, _) -> 
-    | Printin (expr, _) -> [CLOSURE (compile expr)] @ [APPLY] @ [PRINTIN]
-    |  
+    | IfThenElse (cond, a, b, _) -> failwith "not implemented" 
+    | Printin (expr, _) -> [CLOSURE (compile expr)] @ [APPLY] @ [PRINTIN] 
     | _ -> failwith "not implemented"
 
 let e2 = In(Const 1, Const 2, Lexing.dummy_pos)
 
-(* problem with env : the one of pierre uses keys, the one for secd machine is more like a stack. so.. stack ? *)
+(* problem with env : the one of pierre uses keys, the one for secd machine sometimes looks more like a stack. so for let and endlet i don't know what to do yet *)
 (* all code must take into account we can modify the code to be executed next with RETURN *)
+(* dans ma stack il y a :
+*  - du code (instr list)
+*  - des closures (e * code)
+*  - des constantes *)
 
-(*
-let rec exec code dump stack env =
+type stack_items = Code of code | CLOS of string*code*(int Env.t) | Cst of int
+
+
+let rec exec s e code  =
     match code with 
-    | [] -> [] dump stack env
-    | x::xs -> exec_instr x dump stack env ; exec xs dump stack env
-
-and exec_instr i dump stack env =
-    match i with
-    | C k -> stack.push k
-    | BOP binOp -> let u, v = stack.pop, stack.pop in stack.push @@ u binOp v (* assuming u, v are integers ... *)
-    | ACCESS x -> let o = Env.get_most_recent env x in (* assuming (for the moment) it's only int in env *) 
-                  stack.push o
-    | CLOSURE of c -> failwith "not implemented" (* make a copy of the env, push it on the stack c[e] *)
-    (* | APPLY -> ?????? pop code, execute it in old env, but how to do that properly ??????? *)
-    (* | LET -> let elem = stack.pop in Env.add env //what_key ? maybe brunjin indices// elem *)
-    (* | ENDLET -> env.pop *) (* env must be a stack *)
-    | RETURN -> let v = stack.pop in 
-*) 
+    | [] -> Printf.sprintf "last element in s : %s" @@ string_of_int (pop stack)
+    | instr::c ->
+        begin
+        match instr with
+        | C k -> push (Cst k) s ; exec s e c
+        | BOP binOp -> let n2, n1 = pop stack, pop s in
+                       begin 
+                         match n1, n2 with
+                         | (C i, C j) -> push ( C (i binOp j) ) s ; exec s e c
+                         | failwith "wrong type for binop operation"
+                       end
+        | ACCESS x -> begin 
+                        let o = Env.get_most_recent e x in push o s ;
+                        stack, e, c
+                      end
+        | CLOS (x, c') -> push (Clos (x, c', e)) stack; exec s e c
+        | APPLY -> let Clos(x, c', e'), v = pop stack, pop s in 
+                   begin
+                     push e stack;
+                     push c stack;
+                     let e' = Env.add e' x v in exec s e' c'
+                   end
+        | RET -> let v, c', e' = pop stack, pop stack, pop s in 
+                 begin
+                   push v s;
+                   exec s e' c'
+                 end
+        | PRINTIN -> let v = pop s in 
+                     begin
+                       match v with
+                        | Const k -> begin
+                                       Printf.sprintf "%s" (string_of_int k) ;
+                                       push v s ;
+                                       exec s e c
+                                     end
+                        | _ -> failwith "can't printin else than Const int"
+                     end
+        (*| LET -> *) 
+        (* | ENDLET -> e.pop *) (* e must be a s *)
+        | _ -> failwith "not implemented"
