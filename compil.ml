e3e9a525072f1f52a4870fcc33f349f3f80bfe26
gduboc <guillaume.duboc@ens-lyon.fr>
2017-04-09 21:32:11 +0200

    added refs to compil

--- compil.ml
+++ compil.ml
@@ -17,6 +17,8 @@ type instr =
     | PRINTIN (* affiche le dernier élément sur la stack, ne la modifie pas *)
     | BRANCH
     | PROG of code
+    | REF of int ref
+    | BANG of string
 and code = instr list
 
 
@@ -27,19 +29,21 @@ let rec print_code code =
 
 and print_instr i =
     match i with
-      | C k -> Printf.sprintf "CONST(%s); " @@ string_of_int k
-      | BOP bop -> bop # symbol ^ "; "
-      | ACCESS s -> Printf.sprintf "ACCESS(%s); " s
-      | CLOSURE (x, c) -> Printf.sprintf "CLOSURE(%s, %s); " x (print_code c)
-      | CLOSUREC (x, x', c) -> Printf.sprintf "CLOSUREC(%s, %s, %s); " x x' (print_code c) 
-      | LET x -> Printf.sprintf "LET %s; " x
-      | ENDLET -> Printf.sprintf "ENDLET; "
-      | RETURN -> Printf.sprintf "RETURN; "
-      | APPLY -> Printf.sprintf "APPLY; "
-      | PRINTIN -> Printf.sprintf "PRINTIN; "
-      | BRANCH -> Printf.sprintf "BRANCH; "
-      | PROG c -> Printf.sprintf "PROG (%s); " (print_code c)
-
+      | C k -> Printf.sprintf " CONST(%s);" @@ string_of_int k
+      | BOP bop -> " " ^ bop # symbol ^ ";"
+      | ACCESS s -> Printf.sprintf " ACCESS(%s);" s
+      | CLOSURE (x, c) -> Printf.sprintf " CLOSURE(%s, %s);" x (print_code c)
+      | CLOSUREC (x, x', c) -> Printf.sprintf " CLOSUREC(%s, %s, %s);" x x' (print_code c) 
+      | LET x -> Printf.sprintf " LET %s;" x
+      | ENDLET -> Printf.sprintf " ENDLET;"
+      | RETURN -> Printf.sprintf " RETURN;"
+      | APPLY -> Printf.sprintf " APPLY;"
+      | PRINTIN -> Printf.sprintf " PRINTIN;"
+      | BRANCH -> Printf.sprintf " BRANCH;"
+      | PROG c -> Printf.sprintf " PROG(%s);" (print_code c)
+      | REF k -> Printf.sprintf " REF(%s);" (string_of_int !k)
+      | BANG x -> Printf.sprintf " BANG %s;" x
+      | _ -> Printf.sprintf "not implemented;"
 
 let rec compile expr =
   begin
@@ -52,6 +56,10 @@ let rec compile expr =
   | Unit -> []
 
   | Ident (id, _) -> [ACCESS id]
+
+  | Ref (Const k, _) -> let r = ref k in [REF r]
+
+  | Bang(Ident(x, _), _) -> [BANG x]
 
   | BinOp (op, e1, e2, _) -> 
       (compile e2) @ 
@@ -76,7 +84,9 @@ let rec compile expr =
         | _ -> failwith "let rec must define a function"
       end
 
-  | (Call(a,b, _) | Seq(a, b, _) | In(a, b, _)) -> 
+  | Seq(a, b, _) -> (compile a) @ (compile b)
+
+  | (Call(a,b, _) | In(a, b, _)) -> 
       begin
         match a with
         | Let(Ident(x, _), expr, _) -> (compile expr) @ [LET x] @ (compile b) @ [ENDLET] 
