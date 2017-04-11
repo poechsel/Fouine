72da6a03f28cb24a3f09eb47c276efc27fd640fa
poechsel <pierre.oechsel@gmail.com>
2017-04-11 22:00:39 +0200

    cleand inference

--- inference.ml
+++ inference.ml
@@ -4,10 +4,9 @@ open Errors
 open Lexing
 
 
-
+(* print a type *)
 let rec print_type t = 
   let tbl = Hashtbl.create 1 in
-
   let rec aux t = 
     match t with
     | Int_type -> "int"
@@ -17,7 +16,7 @@ let rec print_type t =
     | Unit_type -> "unit"
     | Var_type x -> begin
         match (!x) with
-        | No_type y -> 
+        | No_type y ->                      (* a bit long, because we are trying to mimic the formating of caml *)
           if not (Hashtbl.mem tbl y) then 
             Hashtbl.add tbl y (Hashtbl.length tbl); 
           let id = Hashtbl.find tbl y
@@ -38,40 +37,41 @@ let rec print_type t =
 
   in aux t
 
-let rec prune t d = 
-  if d then Printf.printf "prune %s\n" (print_type t) else ();
+(* pruning: when a Var_type is affected to a type, we must remove the Var_type to keep only the type *)
+let rec prune t  = 
   match t with
-  | Ref_type x -> Ref_type (prune x d)
-  | Fun_type (a, b) -> Fun_type (prune a d, prune b d)
+  | Ref_type x -> Ref_type (prune x)
+  | Fun_type (a, b) -> Fun_type (prune a, prune b)
   | Var_type x -> begin
       match (!x) with 
       | No_type _ -> t
-      | _ -> x := prune !x d; !x
+      | _ -> x := prune !x; !x
     end
   | _ ->  t 
-
+(* check if a base term occurs in another one *)
 let rec occurs_in v t = 
-  let t = prune t false in
+  let t = prune t in
   match v, t with
   | _, Ref_type y -> occurs_in v y
   | Int_type, Int_type -> true
   | Bool_type, Bool_type -> true
   | Array_type, Array_type -> true
   | Unit_type, Unit_type -> true
-  | _, Var_type x -> occurs_in v (prune !x false)
-  | _, Fun_type (a, b) -> occurs_in v (prune a false) || occurs_in v (prune b false)
+  | _, Var_type x -> occurs_in v (prune !x )
+  | _, Fun_type (a, b) -> occurs_in v (prune a ) || occurs_in v (prune b )
   | _ -> false
 
+(* are we in a false (or simulated) function call which as the only goal of checking if a function as a certain type ? *)
 let rec is_spec_comp_call expr =
   match expr with
   | SpecComparer _ -> true
   | Call (x, _, _) -> is_spec_comp_call x
   | _ -> false
 
+(* unify two types. It is during this step that polymorhics types are specialized *)
 let rec unify t1 t2 =
-  (*let _ =  Printf.printf "unify %s with %s \n" (print_type t1) (print_type t2 ) in*)
-  let t1 = prune t1 false
-  in let t2 = prune t2 false in
+  let t1 = prune t1 
+  in let t2 = prune t2 in
   match (t1, t2) with
   | Ref_type x, Ref_type y -> Ref_type (unify x y)
   | Int_type, Int_type -> Int_type
@@ -82,8 +82,8 @@ let rec unify t1 t2 =
   | Var_type ({contents = (No_type a)} as x), Var_type ({contents = (No_type b)} as y) ->
     x := !y;
     Var_type x
-  | Var_type x, _ -> if occurs_in t1 t2 then raise (InferenceError (Msg "rec")) else begin x := t2; prune t1 false end
-  | _, Var_type x -> if occurs_in t2 t1 then raise (InferenceError (Msg "rec")) else begin x := t1; prune t2 false end
+  | Var_type x, _ -> if occurs_in t1 t2 then raise (InferenceError (Msg "rec")) else begin x := t2; prune t1 end
+  | _, Var_type x -> if occurs_in t2 t1 then raise (InferenceError (Msg "rec")) else begin x := t1; prune t2 end
   | Fun_type (a, b), Fun_type (a', b') ->
     let a'' = unify a a'
     in let b'' = unify b b'
@@ -91,13 +91,13 @@ let rec unify t1 t2 =
   | _ -> raise (InferenceError (UnificationError))
 
 
-
+(* type analysis. Lot's of code, because we are also checking for errors here*)
 let rec analyse_aux node env =
-  (* Printf.printf "node-> %s\n" (beautyfullprint node);*)
   let env, out = begin
     match node with
     | Unit -> env, Unit_type
     | Bool _ -> env, Bool_type
+                (* a underscore can have any value *)
     | Underscore -> env, Var_type (get_new_pol_type ())
     | Const _ -> env, Int_type 
     | Ident (x, error_infos) ->  begin
@@ -117,6 +117,7 @@ let rec analyse_aux node env =
       end
 
     | SpecComparer x -> env, x
+
     | BinOp (x, a, b, t) ->
       let _, b_type = analyse_aux b env 
       in let _, a_type = analyse_aux a env 
@@ -144,9 +145,6 @@ let rec analyse_aux node env =
           end
       end
 
-    (*let _, a_type = analyse_aux a env 
-      in let _, b_type= analyse_aux b env 
-      in env, x#type_check (unify a_type b_type *)
     | Call(what, arg, error_infos) -> 
       let _, fun_type = analyse_aux what env 
       in let _, arg_type = analyse_aux arg env 
@@ -154,11 +152,11 @@ let rec analyse_aux node env =
       in begin match fun_type with
         | Var_type ({contents = No_type _}) ->
           let _ = unify (Fun_type (arg_type, (Var_type (storage)))) (fun_type) (*can't have error here, we are trying to unify a 'a with something*)
-          in env, prune (Var_type storage) false
+          in env, prune (Var_type storage) 
         | Fun_type (th_type, _) -> begin
             try 
               let _ = unify (Fun_type (arg_type, (Var_type (storage)))) (fun_type)
-              in env, prune (Var_type storage) false
+              in env, prune (Var_type storage) 
             with InferenceError UnificationError ->
               begin 
                 if is_spec_comp_call what then 
@@ -183,6 +181,7 @@ let rec analyse_aux node env =
       in env, Fun_type (arg_type, snd @@ analyse_aux expr env')
     | Fun (_, _, error_infos) ->
       raise (send_inference_error error_infos "A fun argument must be an identifier or a unit type")
+
     | Let(Ident(name, _), what, _ ) -> 
       let _, def_type = analyse_aux what env 
       in Env.add_type env name def_type, def_type
@@ -212,10 +211,8 @@ let rec analyse_aux node env =
         | LetRec(Ident(x, _), _, _) -> env, t
         | _ -> nenv, t
       end 
-    | MainSeq (a, b, _) ->
-      let nenva, _ = analyse_aux a env
-      in analyse_aux b nenva
-    | Seq (a, b, _) ->
+
+     | Seq (a, b, _) | MainSeq (a, b, _) ->
       let nenva, _ = analyse_aux a env
       in analyse_aux b nenva
     | IfThenElse(cond, a, b, error_infos) ->
@@ -233,6 +230,7 @@ let rec analyse_aux node env =
           end
         | _ -> raise (send_inference_error error_infos (Printf.sprintf "The condition of an ifthenelse clause must be of type bool\n  In expression %s" (Format.underline @@ pretty_print_aux cond "  " true)))
       end
+
     | Ref (x, _) ->
       env, Ref_type (snd @@ analyse_aux x env)
 
@@ -254,6 +252,7 @@ let rec analyse_aux node env =
         with InferenceError SpecComparerError ->
           raise (send_inference_error t (Printf.sprintf "aMake constructor requires a int argument, not a %s.\n  In expression: %s" (print_type arg_type) (pretty_print_amake expr "  " true true)))
       end
+    
     | Printin (expr, t) -> begin
         let _, arg_type = analyse_aux expr env 
         in try
@@ -261,16 +260,8 @@ let rec analyse_aux node env =
         with InferenceError SpecComparerError ->
           raise (send_inference_error t (Printf.sprintf "prInt constructor requires a int argument, not a %s.\n  In expression: %s" (print_type arg_type) (pretty_print_prInt expr "  " true true)))
       end
-          (*
-          begin 
-          try 
-              unify Int_type (snd @@ analyse_aux expr env)
-          with InferenceError x ->
-              raise (InferenceError "an array must have an int argument")
-          end ;
-       env, Array_type *)
+
     | ArraySet (id, expr, nvalue, error_infos) ->
-      (*      analyse_aux (Call(Call(Call(SpecComparer(Fun_type(Array_type, Fun_type(Int_type, Fun_type(Int_type, Unit_type)))), id, t), expr, t), nvalue, t)) env*)
       let _, _ = analyse_aux (ArrayItem(id, expr, error_infos)) env
       in let _, tvalue = analyse_aux nvalue env
       in let _ = begin try 
@@ -279,8 +270,8 @@ let rec analyse_aux node env =
              raise (send_inference_error error_infos (Printf.sprintf "Can't affect an expression of type %s to an element of a int Array.\n  In expression: %s" (print_type tvalue) (pretty_print_arrayset node "" true true)))
          end 
       in env, Unit_type
+
     | ArrayItem (id, expr, error_infos) ->
-      (*      analyse_aux (Call(Call(SpecComparer(Fun_type(Array_type, Fun_type(Int_type, Int_type))), id, t), expr, t)) env*)
       let _ = begin try 
           unify Array_type (snd @@ analyse_aux id env)
         with InferenceError UnificationError ->
@@ -299,6 +290,7 @@ let rec analyse_aux node env =
         | Int_type -> env, Var_type (get_new_pol_type ())
         | _ -> raise (InferenceError (Msg "raise"))
       end
+
     | TryWith (try_exp, error, with_exp, error_infos) ->
       let _, ta = analyse_aux try_exp env
       in let env = match error with
@@ -313,23 +305,7 @@ let rec analyse_aux node env =
          end 
       in env, t
 
-    (*    | TryWith (t_exp, Const(er), w_exp, error_infos) ->
-          let _, ta = analyse_aux false t_exp env
-          in let _, tb = analyse_aux false w_exp env
-          in env, unify ta tb
-          | TryWith (t_exp, Ident(x, _), w_exp, error_infos) ->
-          let _, ta = analyse_aux false t_exp env
-          in let _, tb = analyse_aux false w_exp (Env.add_type env x Int_type)
-          in env, unify ta tb
-
-    *)
-
     | _ -> failwith "not implemented"
-  end in env, prune out false
-
-
-
-
-
+  end in env, prune out 
 
 let analyse a b = analyse_aux a b
